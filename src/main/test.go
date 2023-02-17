39fd3316eebebc6ae313c94d6b1e9a4c77418ae7
missingjoe <hqghqg1@163.com>
2023-02-17 22:31:52 +0800

    lab 2D is coming

--- src/main/test.go
+++ src/main/test.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func c() {
-	for {
-		fmt.Println("1111")
-		time.Sleep(100 * time.Millisecond)
-	}
+func c(a []int) {
+	a[0] = 1
 }
 
-func b() {
-	go c()
-	fmt.Println("2222")
+type a struct {
+	ddd []byte
 }
 
 func main() {
-	b()
-	fmt.Println("finished all")
-	time.Sleep(1 * time.Second)
+	b := a{}
+	c := make([]byte, 10)
+	if b.ddd == nil {
+		fmt.Println(111)
+	}
+	fmt.Println(b.ddd)
+
+	for i := 0; i < 10; i++ {
+		c[i] = 1
+	}
+	b.ddd = c
+
+	fmt.Println(b.ddd)
 }
